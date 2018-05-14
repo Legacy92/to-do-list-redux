533c2445c7089cc2112d5de4d7d0fb2ff7d9130f
Legacy92 <colemancorey22@gmail.com>
2018-05-14 12:35:26 -0700

    Redux setup complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
+import { BrowserRouter as Router } from 'react-router-dom';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, {}, applyMiddleware());
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
